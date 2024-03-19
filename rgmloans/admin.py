cd52dc8df16d8ef5239778b808a2f6d725b82c15
ranjitgrewal <ranjitr@hotmail.com>
2024-03-19 16:26:08 -0700

    reverted new upload

--- rgmloans/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, LoanOffer,Application,Loan,ContactNotification
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(LoanOffer)
-admin.site.register(Application)
-admin.site.register(Loan)
-admin.site.register(ContactNotification)
